be22aa318e1ace60080f72eb2c32accd9223f20c
jessebinja3-dot <jessebinja3@gmail.com>
2026-07-25 20:35:01 +0200

    Add files via upload

---
+++ question_2.cpp
@@ -0,0 +1,17 @@
+// Question 2: Student Information
+// This program displays a student's name, age, and programme of study.
+ 
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string name = "Jesse ";
+    int age =  18;
+    string programme = "Bachelor of Science in Computer Science";
+ 
+    cout << "Student Name : " << name << endl;
+    cout << "Student Age  : " << age << endl;
+    cout << "Programme    : " << programme << endl;
+ 
+    return 0;
+}
